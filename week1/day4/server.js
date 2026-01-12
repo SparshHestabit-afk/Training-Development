92b9dc3532c360ad38991d5f8a5440546263ba6b
Sparsh Agarwal <sparsha.hestabit@gmail.com>
2026-01-12 13:35:07 +0530

    Reapply "Day 4- All tasks Completed"
    This reverts commit ad8e86a08315da8ec204310f7ab8783513b477be.

---
+++ week1/day4/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/echo') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(req.headers, null, 2));
+  }
+
+  else if (parsedUrl.pathname === '/slow') {
+    const delay = parseInt(parsedUrl.query.ms) || 1000;
+
+    setTimeout(() => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`Response delayed by ${delay} ms`);
+    }, delay);
+  }
+
+  else if (parsedUrl.pathname === '/cache') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'public, max-age=60',
+      'ETag': 'node-etag-123'
+    });
+    res.end('This response is cacheable');
+  }
+
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
+
